9cb1ca634a18e428dfca84b6a7f370e8d6698656
Hersonmei <herson.me@hotmail.com>
2022-02-27 12:08:22 -0300

    Finalização JS

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
-const question = document.querySelectorAll('.space')
+const space = document.querySelectorAll('.space')
+const question = document.querySelectorAll('.question')
+const answer = document.querySelectorAll('.answer')
+const arrow = document.querySelectorAll('.arrow')
 
 
-question.forEach(item => {item.addEventListener("click", console.log(item))
-} 
-)
+space.forEach((spac, idx) => {
+    spac.addEventListener('click', () => {
+        removeActiveClasse();
+        removeShowClasse();
+
+        question[idx].classList.add('active')
+        answer[idx].classList.add('show')
+        arrow[idx].classList.add('rotate')   
+    })
+})
+
+function removeShowClasse() {
+    answer.forEach(aswr => {
+        aswr.classList.remove('show')
+    })
+}
+
+function removeActiveClasse() {
+    question.forEach(quest => {
+        quest.classList.remove('active')
+    })
+}
+
+function removeRotateClasse() {
+    arrow.forEach(arr => {
+        arr.classList.remove('rotate')
+    })
+}
